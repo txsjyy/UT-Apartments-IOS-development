d0fa92229370c22c48394d5ff98c4d0b24561288
Mingda Li <curryli0420@gmail.com>
2022-11-07 22:50:40 -0600

    Add files via upload

--- Project/ViewController.swift
+++ Project/ViewController.swift
@@ -9,6 +9,7 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var ttt = 1
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
