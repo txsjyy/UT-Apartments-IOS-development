224c62399292b4e20587f8170c2ef5bc1f89b8e3
Junyu Yao <yaojunyu@utexas.edu>
2022-11-11 15:18:39 -0600

    meeting

--- Project/MainViewController.swift
+++ Project/MainViewController.swift
@@ -7,16 +7,25 @@
 
 import UIKit
 
-class MainViewController: UIViewController {
-
+class MainViewController: UIViewController,UITableViewDelegate,UITableViewDataSource {
+    
+    
+    @IBOutlet weak var tableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        tableView.delegate = self
+        tableView.dataSource = self
+        
      
     }
-
-    @IBAction func logoutPressed(_ sender: Any) {
-        self.navigationController?.popToRootViewController(animated: true)
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        <#code#>
     }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
+
     
 }
