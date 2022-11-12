d5ffd5d9423933401de493d09f7823a99ea1b2c9
lmd0420 <curryli0420@gmail.com>
2022-11-12 16:07:16 -0600

    try commit 2

--- Project/ViewController.swift
+++ Project/ViewController.swift
@@ -10,6 +10,7 @@ import FirebaseAuth
 
 class ViewController: UIViewController {
     var test = 1
+    var test2 = 2
     @IBOutlet weak var passwordField: UITextField!
     @IBOutlet weak var IDField: UITextField!
     @IBOutlet weak var SegCrtl: UISegmentedControl!
