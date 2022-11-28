ec2a4a99a2835fefe0f6fb4864a14d8619ee09ee
Junyu Yao <97806768+txsjyy@users.noreply.github.com>
2022-11-28 16:45:33 -0600

    Add files via upload

---
+++ Project/Project/ItemCollectionViewCell.swift
@@ -0,0 +1,20 @@
+//
+//  ItemCollectionViewCell.swift
+//  Project
+//
+//  Created by Ricky on 11/27/22.
+//
+
+import UIKit
+
+class ItemCollectionViewCell: UICollectionViewCell {
+
+    @IBOutlet weak var imageView: UIImageView!
+    
+    @IBOutlet weak var label: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
