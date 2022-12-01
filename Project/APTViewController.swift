601c1a2e292e90c6b897a68cec8b4017ef601ffe
ricky-no1 <rickyno1@utexas.edu>
2022-11-30 18:47:00 -0600

    1

--- Project/APTViewController.swift
+++ Project/APTViewController.swift
@@ -7,12 +7,21 @@
 
 import UIKit
 import MapKit
+import CoreLocation
 
 class APTViewController: UIViewController, MKMapViewDelegate {
     @IBOutlet weak var name_label: UILabel!
+    
+    @IBOutlet weak var price_label: UILabel!
     var delegate1: UIViewController!
+    
+    @IBOutlet weak var distance_label: UILabel!
     @IBOutlet weak var apt_mapView: MKMapView!
     @IBOutlet weak var apt_imageView: UIImageView!
+    
+    let gdc_latitude: Double = 30.28624
+    let gdc_longitude: Double = -97.73653
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -26,11 +35,19 @@ class APTViewController: UIViewController, MKMapViewDelegate {
         
         apt_mapView.setRegion(region, animated: true)
         apt_mapView.mapType = .standard
-
         
+        
+        price_label.text = apartment_list[chosenidex].apt_price
+        
+        
+        let gdc_location = CLLocation(latitude: gdc_latitude, longitude: gdc_longitude)
+        let apt_location = CLLocation(latitude: apartment_list[chosenidex].apt_latitude, longitude: apartment_list[chosenidex].apt_longitude)
+        
+        
+        let distanceInMeters = gdc_location.distance(from: apt_location)
+        distance_label.text = String(format: "%.2f", distanceInMeters) + "m"
     }
     
 
    
 }
-
